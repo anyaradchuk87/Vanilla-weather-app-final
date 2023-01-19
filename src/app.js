f37665bbd0127da79cabed3a96ab571f2402f76d
Anna Radchuk <108876851+anyaradchuk87@users.noreply.github.com>
2023-01-19 23:24:21 +0000

    search working

---
+++ src/app.js
@@ -0,0 +1,59 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let dayIndex = date.getDay();
+  let day = days[dayIndex];
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector(".search-input");
+  let city = document.querySelector("#city");
+  console.log(searchInput);
+  city.innerHTML = searchInput.value;
+}
+
+function showWeather(response) {
+  let h1 = document.querySelector("h1");
+  let temperature = Math.round(response.data.main.temp);
+  h1.innerHTML = `It is ${temperature}°C in ${response.data.name}`;
+}
+
+let apiKey = "a8c22b5699a15a6f6db70d81d7aec97a";
+let city_name = "San Francisco";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
+
+let currentDate = new Date();
+let date = document.querySelector("#date");
+date.innerHTML = formatDate(currentDate);
+
+let searchForm = document.querySelector("#search");
+searchForm.addEventListener("click", searchCity);
+
+// navigator.geolocation.getCurrentPosition(findPosition);
