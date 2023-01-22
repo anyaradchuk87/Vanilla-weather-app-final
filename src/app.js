753253384785a122e03510b6a6190f86948ee3db
Anna Radchuk <108876851+anyaradchuk87@users.noreply.github.com>
2023-01-22 15:01:49 +0000

    update time

--- src/app.js
+++ src/app.js
@@ -5,13 +5,16 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
-function formatDate(date) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
   let days = [
     "Sunday",
     "Monday",
@@ -21,8 +24,7 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  let dayIndex = date.getDay();
-  let day = days[dayIndex];
+  let day = days[date.getDay()];
 
   let hours = date.getHours();
   if (hours < 10) {
@@ -33,7 +35,6 @@ function formatDate(date) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-
   return `${day} ${hours}:${minutes}`;
 }
 
@@ -45,23 +46,13 @@ function searchCity(event) {
   city.innerHTML = searchInput.value;
 }
 
-function showWeather(response) {
-  let h1 = document.querySelector("h1");
-  let temperature = Math.round(response.data.main.temp);
-  h1.innerHTML = `It is ${temperature}°C in ${response.data.name}`;
-}
-
 let apiKey = "a8c22b5699a15a6f6db70d81d7aec97a";
 let city_name = "San Francisco";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
 
-let currentDate = new Date();
-let date = document.querySelector("#date");
-date.innerHTML = formatDate(currentDate);
-
-let searchForm = document.querySelector("#search");
-searchForm.addEventListener("click", searchCity);
+// let searchForm = document.querySelector("#search");
+// searchForm.addEventListener("click", searchCity);
 
 // navigator.geolocation.getCurrentPosition(findPosition);
